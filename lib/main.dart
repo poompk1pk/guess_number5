53d3dac007e3ceca88b824eabbb98aabf3ae5624
PoomPK <poompk2542@gmail.com>
2022-01-14 14:32:52 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'game.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -23,8 +25,9 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final _controller = TextEditingController();
-
+  final _controller = TextEditingController(); // controller
+  static final default_hint = "ทายเลขตั้งแต่ 1 ถึง 100";
+  Game _game = Game();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,16 +80,16 @@ class HomePage extends StatelessWidget {
                     SizedBox(width: 8.0),
                     Column(
                       mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start, // ปรับให้ตรงกันชิดซ้าย
                       children: [
                         Text('GUESS',
                             style: TextStyle(
-                                fontSize: 36.0, color: Colors.purple.shade200)),
+                                fontSize: 36.0, color: Colors.orange.shade200)),
                         Text(
                           'THE NUMBER',
                           style: TextStyle(
                             fontSize: 18.0,
-                            color: Colors.purple.shade600,
+                            color: Colors.deepOrange.shade600,
                             //fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -100,6 +103,11 @@ class HomePage extends StatelessWidget {
                 child: TextField(
                   textAlign: TextAlign.center,
                   controller: _controller,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white, filled: true,
+                    border: OutlineInputBorder(),
+                    hintText: default_hint,
+                  ),
                 ),
               ),
               Padding(
@@ -108,14 +116,37 @@ class HomePage extends StatelessWidget {
                   child: Text('GUESS'),
                   onPressed: () {
                     var input = _controller.text;
+                    int input_number;
+                    var title = 'RESULT';
+                    var content = 'กรอกข้อมูลไม่ถูกต้อง ให้กรอกเฉพาตัวเลขเท่านั้น';
+                    try {
+                      input_number = int.parse(input);
 
+                      // Do
+                      var result = _game.doGuess(input_number);
+
+                      if(result==1){
+                        content = '$input_number มากเกินไป กรุณาลองใหม่';
+                      } else if(result==-1){
+                        content = '$input_number น้อยเกินไป กรุณาลองใหม่';
+                      } else {
+                        title = 'RESULT';
+                        var count = _game.guessCount;
+                        content = '$input_number ถูกต้องครับ\nคุณทายทั้งหมด $count ครั้ง';
+                        _game = Game();
+                      }
+
+                    } catch(e) {
+                      title = 'ERROR';
+                    }
+                    _controller.clear();
                     showDialog(
                       context: context,
                       barrierDismissible: false,
                       builder: (BuildContext context) {
                         return AlertDialog(
-                          title: Text('RESULT'),
-                          content: Text(input),
+                          title: Text(title),
+                          content: Text(content),
                           actions: [
                             TextButton(
                               onPressed: () {
